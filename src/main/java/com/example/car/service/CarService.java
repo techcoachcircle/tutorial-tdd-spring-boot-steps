ad790cec82c18d934394680267d774163ca2d693
vivganes <vivek@vivekganesan.com>
2022-08-28 11:45:38 +0530

    5_passing_service_test_but_failing_contract_test

--- src/main/java/com/example/car/service/CarService.java
+++ src/main/java/com/example/car/service/CarService.java
@@ -1,13 +1,21 @@
 package com.example.car.service;
 
 import com.example.car.model.Car;
+import com.example.car.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CarService {
 
+    @Autowired
+    CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
     public List<Car> getCars() {
-        return new ArrayList<>();
+        return carRepository.getAll();
     }
 }
