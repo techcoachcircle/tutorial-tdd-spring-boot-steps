5e4190fa2feca6ab5f6a22a9f9a9b26118f95b56
vivganes <vivek@vivekganesan.com>
2022-08-28 14:31:28 +0530

    9_passing_contract_test

--- src/main/java/com/example/car/service/CarService.java
+++ src/main/java/com/example/car/service/CarService.java
@@ -4,9 +4,12 @@ import com.example.car.model.Car;
 import com.example.car.repository.CarRepository;
 import org.apache.commons.collections.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
 public class CarService {
 
     @Autowired
--- src/test/java/com/example/car/CarBase.java
+++ src/test/java/com/example/car/CarBase.java
@@ -1,9 +1,12 @@
 package com.example.car;
 
+import com.example.car.model.Car;
+import com.example.car.repository.CarRepository;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -13,6 +16,16 @@ public class CarBase {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Transactional
+    @BeforeEach
+    public void addData () {
+        carRepository.save(new Car("prius", "hybrid"));
+        carRepository.save(new Car("tesla", "electric"));
+    }
 
     @BeforeEach
     public void initialiseRestAssuredMockMvcWebApplicationContext() {
