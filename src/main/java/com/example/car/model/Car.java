30726a91cee31b7f0ea61ea3c2cd1fc69c439f7e
vivganes <vivek@vivekganesan.com>
2022-08-28 13:33:32 +0530

    7_passing_repo_first_test_for_autogenerate_id

--- src/main/java/com/example/car/model/Car.java
+++ src/main/java/com/example/car/model/Car.java
@@ -1,12 +1,14 @@
 package com.example.car.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Car {
 
     @Id
+    @GeneratedValue
     private Long id;
     private String name;
     private String type;
